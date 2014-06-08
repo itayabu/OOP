6aa8f52c9d96c9a5e893495e40ce294ed956849d
AssafHMor <rammenn@gmail.com>
2014-06-08 16:39:27 +0300

    please run

--- ex7/src/Dummy.java
+++ ex7/src/Dummy.java
@@ -1,12 +1,12 @@
 
 public class Dummy {
-
+	String itay = "ITAY"
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(itay);
 	}
 
 }
