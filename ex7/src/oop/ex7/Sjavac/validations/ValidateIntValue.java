58e0c7ad693d1698d6550f24326ee8ac033a8f17
AssafHMor <rammenn@gmail.com>
2014-06-18 12:49:45 +0300

    commit

--- ex7/src/oop/ex7/Sjavac/validations/ValidateIntValue.java
+++ ex7/src/oop/ex7/Sjavac/validations/ValidateIntValue.java
@@ -1,6 +1,7 @@
 package oop.ex7.Sjavac.validations;
 
 import java.util.ArrayList;
+
 import oop.ex7.Sjavac.Type;
 import oop.ex7.Sjavac.exception.IllegaIntException;
 import oop.ex7.Sjavac.instance.*;
@@ -35,7 +36,7 @@ public class ValidateIntValue {
 			
 			//if the string might be a name of an initialized member
 			if (s.matches(INT_VAL_STR))
-				return checkIfInList(list, s);
+				return Type.checkIfInList(list, s,Type.INT);
 			
 			String[] str = null;
 			//if the string might contain an int and an initialized member
@@ -43,32 +44,17 @@ public class ValidateIntValue {
 				str = s.split("[+-/*]");//split the string between the actions
 				//if the first substring is a member
 				if(str[0].matches(INT_VAL_STR)&&str[1].matches("\\d*"))
-					return checkIfInList(list,str[0]);
+					return Type.checkIfInList(list,str[0],Type.INT);
 				//if the second substring is a member
 				if(str[1].matches(INT_VAL_STR)&&str[0].matches("\\d*"))
-					return checkIfInList(list, str[1]);
+					return Type.checkIfInList(list, str[1],Type.INT);
 						
 			}	
 			if (s.matches(INT_VAL_STR+"[+-/*]"+INT_VAL_STR))
 				str = s.split("[+-/*]");
-				return(checkIfInList(list, str[0])&&checkIfInList(list, str[1]));
+				return(Type.checkIfInList(list, str[0],Type.INT)&&Type.checkIfInList(list, str[1],Type.INT));
 		}
 		
-		throw IlegalIn
-	}
-	/**
-	 * check if list contains an instance with the same name
-	 * @param list the list to check if contains
-	 * @param str
-	 * @return
-	 */
-	private static boolean checkIfInList (ArrayList<ArrayList<Instance>> list ,String str){
-		for(ArrayList<Instance> instArray:list)
-			for(Instance inst:instArray)
-				if(inst.getType().equals(str))
-					return(inst.getType().equals(Type.INT));
-		return false;
-	}
-	
-	
+		throw new IllegaIntException("");
+	}	
 }
