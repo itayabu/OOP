80e41119cd1ade64c3053775ffaaa3200ede5f5d
AssafHMor <rammenn@gmail.com>
2014-06-18 21:42:51 +0300

    com

--- ex7/src/oop/ex7/main/validations/ValidateIntValue.java
+++ ex7/src/oop/ex7/main/validations/ValidateIntValue.java
@@ -2,6 +2,7 @@ package oop.ex7.main.validations;
 
 import java.util.ArrayList;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import oop.ex7.main.Type;
 import oop.ex7.main.exceptions.IllegaIntException;
@@ -12,7 +13,7 @@ public class ValidateIntValue {
 	private static final String INT_VAL_FORM = "-?\\d*";
 	//the form of an preinitialized type
 	private static final String INT_VAL_STR = "_?\\[a-zA-z]+\\w+?";
-	
+
 	/**
 	 * 
 	 * @param list
@@ -26,19 +27,19 @@ public class ValidateIntValue {
 
 		//check if the type is really of an int Type
 		if (type.getType().equals("int")){
-			
+
 			//if the the string is an int
 			if (s.matches(INT_VAL_FORM))
 				return true;
-			
+
 			//if the string is an action between two integers
 			if (s.matches("-?\\d*[*/+-]-?\\d*"))
 				return true;
-			
+
 			//if the string might be a name of an initialized member
 			if (s.matches(INT_VAL_STR))
 				return Type.checkIfInList(list, s,Type.INT);
-			
+
 			String[] str = null;
 			//if the string might contain an int and an initialized member
 			if (s.matches(INT_VAL_FORM+"[*/+-]"+INT_VAL_STR)||s.matches(INT_VAL_STR+"[+-/*]"+INT_VAL_FORM)){
@@ -48,15 +49,15 @@ public class ValidateIntValue {
 					return Type.checkIfInList(list,str[0],Type.INT);
 				//if the second substring is a member
 				if(str[1].matches(INT_VAL_STR)&&str[0].matches("\\d*"))
-					return Type.checkIfInList(list, str[1],Type.INT);
-						
-			}	
-			if (s.matches("_?[a-zA-z]*?\\w+[\\*/-\\+]_?[a-zA-z]+\\w*?")){
+					return Type.checkIfInList(list, str[1],Type.INT);	
+			}try{
+			if (s.matches("_?[a-zA-z]+\\w+[*/+-]_?[a-zA-z]+\\w+")){
 				str = s.split("[+-/*]");
 				return(Type.checkIfInList(list, str[0],Type.INT)&&Type.checkIfInList(list, str[1],Type.INT));
 			}
-			else
-				return false;
+			}catch (PatternSyntaxException e){
+				System.err.println("not good");
+			}
 		}
 		return false;
 	}	
