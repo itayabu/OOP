af96a4b7059422d6ff3dd0875c398d6b8cd87182
AssafHMor <rammenn@gmail.com>
2014-06-21 13:14:54 +0300

    commit

--- ex7/src/oop/ex7/main/validations/InstanceArrayValidator.java
+++ ex7/src/oop/ex7/main/validations/InstanceArrayValidator.java
@@ -7,12 +7,14 @@ import oop.ex7.main.instance.Instance;
 public class InstanceArrayValidator {
 
 	/**
-	 * check if already exist instance in block
-	 * @param checkInstance the instance to check
-	 * @param instanceInBlock all instances already exist in current block
+	 * check if instance already exist in block
+	 * @param checkInstance - the instance to check
+	 * @param instanceInBlock - all instances already exist in current block
 	 * @return
 	 */
-	public static boolean instanceNameExistInBlock(Instance checkInstance, ArrayList<Instance> instanceInBlock){
+	public static boolean instanceNameExistInBlock(Instance checkInstance, 
+			ArrayList<Instance> instanceInBlock){
+
 		for (Instance instance:instanceInBlock){
 			if (checkInstance.getName().equals(instance.getName())){
 				return true;
@@ -20,6 +22,7 @@ public class InstanceArrayValidator {
 		}
 		return false;
 	}
+	
 	/**
 	 * check if instance with a given name exist in code.
 	 * check in a given method list and in the main list
@@ -27,7 +30,9 @@ public class InstanceArrayValidator {
 	 * @param s
 	 * @return instance with the given name if exist, null else.
 	 */
-	public static Instance findInstance(ArrayList<ArrayList<Instance>> list, String s){
+	public static Instance findInstance(ArrayList<ArrayList<Instance>> list, 
+			String s){
+		
 		for (ArrayList<Instance> subList: list){
 			for (Instance instance: subList){
 				if (instance.getName().equalsIgnoreCase(s)){
